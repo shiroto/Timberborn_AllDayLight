e2923220900cb4b401d2d87e6dd0a1546e841a0f
marco-knietzsch <71555548+marco-knietzsch@users.noreply.github.com>
2024-11-28 11:52:24 +0100

    init

---
+++ Timberborn_AllDayLight/ModStarter.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using Timberborn.ModManagerScene;
+
+namespace Timberborn_AllDayLight
+{
+    public class ModStarter : IModStarter
+    {
+        public void StartMod(IModEnvironment modEnvironment)
+        {
+            Harmony harmony = new("com.marcok.alldaylight");
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ Timberborn_AllDayLight/Patches.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Timberborn.SkySystem;
+
+namespace Timberborn_AllDayLight
+{
+    [HarmonyPatch]
+    public class Patches
+    {
+        [HarmonyPatch(typeof(Sun), "UpdateColors", typeof(DayStageTransition))]
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
